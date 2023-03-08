b3d9a7f1f351e481c40fc9e019c12f43ef7a8a58
seokmin <essemfly@gmail.com>
2023-03-08 16:26:41 +0900

    Clean code

--- lib/repository/api_calls.dart
+++ lib/repository/api_calls.dart
@@ -1,14 +1,7 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:handover_app/initializers/app.dart';
-import 'package:handover_app/utils.dart';
-
 import 'api_manager.dart';
 
 export 'api_manager.dart' show ApiCallResponse;
-
-const _kPrivateApiFunctionName = 'ffPrivateApiCall';
 
 const _baseApiUrl = 'http://52.79.251.236';
 // const String _baseApiUrl = 'http://127.0.0.1:8000';
